bb91d678568d30b700eb6201fa14ebb53ec38ddf
GesaLutfien <gesavlop@gmail.com>
2025-08-23 23:13:12 +0700

    feat: initial

---
+++ bundle/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nama = "Geisha Lutfan Waldhani";
+let age = 17;
+let isStudent = true;
+let hobies;
+hobies = ["Reading", "PlayingGame", "Coding"];
+console.log({ nama });
+console.log({ age });
+console.log({ isStudent });
+console.log({ hobies });
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let nama: string = "Geisha Lutfan Waldhani"
+let age: number = 17
+let isStudent: boolean = true
+let hobies:string[]
+
+hobies = ["Reading", "PlayingGame", "Coding"]
+
+console.log({nama})
+console.log({age})
+console.log({isStudent})
+console.log({hobies})
